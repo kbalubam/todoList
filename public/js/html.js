492385e42f4090df2ea752b91ccd81bd17eba6d3
kbalubam <kevin.baluba@hotmail.com>
2021-06-14 23:57:28 +0200

    vf probleme clear list button

--- public/js/html.js
+++ public/js/html.js
@@ -51,10 +51,125 @@ aAll.appendChild(buttonAll);
 aDone.appendChild(buttonDone);
 aTodo.appendChild(buttonTodo);
 
+let clearButton = document.createElement("button");
+clearButton.setAttribute("class", "clear");
+section.appendChild(clearButton);
+clearButton.innerText="CLEAR LIST"
 
-// 
-let divCheck = document.createElement("div");
-let divLabel = document.createElement("div");
-let divIcon = document.createElement("div");
+
+let valeur;
+
+inputTask.addEventListener("keyup",  (e)=> {
+    if (e.keyCode == 13) {
+        valeur=inputTask.value;
+        inputTask.value="";
+        let divGlob = document.createElement("div");
+        divGlob.setAttribute("class", "divGlob");
+        let divLabel = document.createElement("div");
+        divLabel.setAttribute("class","divLabel");
+        let divIcon = document.createElement("div");
+        divIcon.setAttribute("class", "divIcon");
+
+        let inputCheck = document.createElement("input");
+        inputCheck.setAttribute("type","checkbox");
+        inputCheck.setAttribute("name", "checkbox")
+        let label = document.createElement("label");
+        label.textContent=`${valeur}`;
+        let icon1 = document.createElement("i");
+        icon1.setAttribute("class","far fa-edit");
+        let icon2 = document.createElement("i");
+        icon2.setAttribute("class","fas fa-trash-alt");
+
+        let bEdit=document.createElement("a");
+        bEdit.setAttribute("class", "bEdit");
+        bEdit.setAttribute("href", "#");
+        
+        let bDelete=document.createElement("a");
+        bDelete.setAttribute("class", "bDelete");
+        bDelete.setAttribute("href", "#");
+
+        section.appendChild(divGlob);
+        divGlob.appendChild(divLabel);
+        divGlob.appendChild(divIcon);
+        
+        divLabel.appendChild(inputCheck);
+        divLabel.appendChild(label);
+        divIcon.appendChild(bEdit);
+        divIcon.appendChild(bDelete);
+        
+        bEdit.appendChild(icon1);
+        bDelete.appendChild(icon2);
+
+        
+
+        let checkbox = document.querySelector("input[name=checkbox]");
+
+        checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            divGlob.style.backgroundColor="rgba(52,58,64,255)"
+        } else {
+            divGlob.style.backgroundColor="white"
+        }
+        });
+        buttonAll.addEventListener("click", () => {
+           if (inputCheck.checked == true || inputCheck.checked == false){
+               divGlob.style.display = "flex";
+           }
+        });
+
+        buttonDone.addEventListener("click", () => {
+           if (inputCheck.checked == true){
+               divGlob.style.display = "flex";
+             } else {
+               divGlob.style.display = "none";
+             }
+        });
+        buttonTodo.addEventListener("click", () => {
+            if (inputCheck.checked == false){
+                divGlob.style.display = "flex";
+                console.log("hello");
+              } else {
+                divGlob.style.display = "none";
+                console.log("hello");
+              }
+         });
+
+         bEdit.addEventListener("click", () =>{
+            label.innerText = "";
+            let newinput = document.createElement("input");
+            divGlob.appendChild(newinput);
+            newinput.addEventListener("keyup", (e) =>{
+                          if(e.keyCode == 13){
+                             label.innerText = newinput.value
+                             newinput.remove()
+                          }
+            })
+         });
+
+         bDelete.addEventListener("click",()=>{
+             divGlob.remove();
+         })
+    
+    
+
+        
+
+    }
+})
+
+let element = document.querySelectorAll(".divGlob")
+clearButton.addEventListener("click",(element)=>{
+    if(element.classList=="divGlob"){
+        element.remove();
+    }
+})
+
+
+
+
+
+
+
+
 
 
